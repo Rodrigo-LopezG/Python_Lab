fa2b9214d1326126788f1aecf38d309ad1772d12
Rodrigo-LopezG <rodrigo_50@hotmail.com>
2026-06-23 20:49:32 -0600

    Create __init__.py with module documentation
    Added module docstring for Lab 05 package.

---
+++ Laboratorios/Lab05/src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Laboratorio 05: Type Hints y Calidad de Código Python
+
+Este paquete contiene ejemplos y ejercicios sobre:
+- Type hints y typing avanzado
+- Verificación estática con mypy
+- Calidad de código con PEP 8
+- Integración continua con pre-commit
+"""
